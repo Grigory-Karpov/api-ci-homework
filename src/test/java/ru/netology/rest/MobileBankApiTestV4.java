0404238a9248194f68a8404c17e1691bd5008322
Grigory <gregkarpov64@gmail.com>
2025-10-24 22:17:37 +0400

    feat: Add JSON schema validation to V4 test

--- src/test/java/ru/netology/rest/MobileBankApiTestV4.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV4.java
@@ -8,16 +8,12 @@ import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInC
 class MobileBankApiTestV4 {
     @Test
     void shouldReturnDemoAccounts() {
-        // Given - When - Then
-        // Предусловия
         given()
-                .baseUri("http://localhost:9999/api/v1")
-                // Выполняемые действия
+                .baseUri("http://localhost:9999")
+                .log().all()
                 .when()
-                .get("/demo/accounts")
-                // Проверки
+                .get("/api/v1/demo/accounts")
                 .then()
-                // Включаем логирование ответа
                 .log().all()
                 .statusCode(200)
                 .body(matchesJsonSchemaInClasspath("accounts.schema.json"));
